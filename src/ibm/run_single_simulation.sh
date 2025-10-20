25df7a647de264687d96237bfd70fcf90fe03b6d
Bram Kuijper <bramkuijper@gmail.com>
2025-10-20 21:29:30 +0100

    adding an additional parameter to run_single_simulation.sh

--- src/ibm/run_single_simulation.sh
+++ src/ibm/run_single_simulation.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-# exe name      a       b       c       biast       mu_p    mu_t    sdmu_p      sdmu_t      init_p  init_t   file name
-./fisher_sexsel.exe   1.0     0.0025    0.5     0.99        0.05    0.05    0.4        0.4   1.0  0.0     output_fisher.csv 
+# exe name      a       b       c       biast       mu_p    mu_t    sdmu_p      sdmu_t      init_p  init_t  only_pos    file name
+./fisher_sexsel.exe   1.0     0.0025    0.5     0.99        0.05    0.05    0.4        0.4   1.0    0.0     0           output_fisher.csv 
